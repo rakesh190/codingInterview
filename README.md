3642e689e4b90f0e5b592a12d4b9102f206bae96
rgoudbob <rgoudbob@cisco.com>
2018-07-05 17:37:47 -0700

    Added Reverse a String code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # codingInterview
+
+1) Reverse a String
+   Included following 3 solutions to reverse a String
+   1) By reverse() method.
+   2) By using for loop
+   3) By using reduce() method
---
+++ reverseAString/stringReverse.js
@@ -0,0 +1,27 @@
+'use strict'
+
+function reverseAStringByMethod(string){
+    return string
+        .split('')
+        .reverse()
+        .join('');
+}
+console.log(reverseAStringByMethod('apple')) // elppa
+
+function reverseStringByForLoop(string){
+    let reversedString = '';
+    for(let i of string){
+        reversedString = i + reversedString;
+    }
+    return reversedString;
+}
+
+console.log(reverseStringByForLoop('rakesh'));
+
+function reverseAStringByReduceMethod(string){
+     string.split('').reduct((reversed, char)=> {
+        return char+reversed;
+    }, '');
+}
+
+console.log(reverseStringByForLoop('santhos'))
